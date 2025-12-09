8a529d8240705816c8e735e15278134c5e456a9c
Crazeah <183840676+Crazeah@users.noreply.github.com>
2025-12-09 22:59:31 +0800

    feat: 完成活動建立、列表、審核詳情頁面 (Bootstrap)

---
+++ src/main/java/com/dive/club/DiveClubApplication.java
@@ -0,0 +1,13 @@
+package com.dive.club;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiveClubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiveClubApplication.class, args);
+	}
+
+}
